06820aef30a34da3581f8e4897f92724f75c0329
Julio Daniel/JulioDev30 <juliodev30+github@gmail.com>
2026-09-17 13:21:51 -0300

    feat(events): implement event taxonomy and wire up application dispatcher

---
+++ engine/include/engine/events/event.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file event.hpp
+ * @author Julio Daniel
+ * @brief ${END}
+ * @date 17/09/2026
+ *
+ * Copyright (c) 2026 Julio Daniel. All rights reserved. / Todos os direitos reservados.
+ */
+#ifndef HELLIXENGINE_EVENT_HPP
+#define HELLIXENGINE_EVENT_HPP
+
+#endif //HELLIXENGINE_EVENT_HPP
