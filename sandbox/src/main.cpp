de25a04e3e4f2e3142d0f41f718b986369e45920
Julio Daniel/JulioDev30 <juliodev30+github@gmail.com>
2026-09-24 21:46:33 -0300

    build(cmake): link gcc runtime statically on mingw

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -7,15 +7,16 @@
 class SandboxApp : public hlx::Application {
 public:
     SandboxApp() : Application("Hellix Sandbox - Teste de Render 2D") {
+        hlx::FileSystem::unmount("engine://");
+        hlx::FileSystem::mount("engine://", "../engine");
         HLX_INFO("Sandbox iniciado com sucesso!");
         pushLayer(new ExampleLayer());
 
 
-        auto iconPath = hlx::FileSystem::resolve("assets://icon/icon.png");
 
-
-
-        getWindow().setIcon(iconPath.string().c_str());
+        //auto iconPath = hlx::FileSystem::resolve("assets://icon/icon.png");
+       // getWindow().setIcon(iconPath.string().c_str());
+        getWindow().setIcon("assets://icon/icon.png");
 
     }
 
